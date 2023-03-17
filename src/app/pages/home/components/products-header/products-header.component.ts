f203dc719896f941fc2124f6d878e30551c6289c
Oleksii <stinovoleksij@gmail.com>
2023-03-17 18:26:57 +0200

    Added a header and cart icons

---
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-header',
+  templateUrl: './products-header.component.html',
+  styleUrls: ['./products-header.component.css'],
+})
+export class ProductsHeaderComponent {
+  sort = 'descending';
+  itemsShowCount: number = 12;
+
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort;
+  }
+
+  onItemsUpdated(count: number): void {
+    this.itemsShowCount = count;
+  }
+  onColumnsUpdated(colsNum: number) {}
+}
