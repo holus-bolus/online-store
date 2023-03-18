aadd72670ca8fdebd3396ac2492c589309223f4d
Oleksii <stinovoleksij@gmail.com>
2023-03-18 13:50:48 +0200

    Added a cart tab

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Cart, CartItem } from '../../models/cart.model';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
+})
+export class CartComponent implements OnInit {
+  cart: Cart = {
+    items: [
+      {
+        product: 'https://i.insider.com/55a966f3371d22c6178b62ed?width=700',
+        name: 'Sneakers',
+        price: 250,
+        quantity: 1,
+        id: 1,
+      },
+    ],
+  };
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action',
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   cols = 3;
   category: string | undefined;
+  rowHeight = ROWS_HEIGHT[this.cols];
 
   constructor() {}
 
@@ -15,6 +18,7 @@ export class HomeComponent implements OnInit {
 
   onColumnsCountChanged(colsNumber: number): void {
     this.cols = colsNumber;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
   }
 
   onShowCategory(newCategory: string): void {
